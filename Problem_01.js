c3f4af509dcd2aac5c3e7c8ab38b2999f090f175
Shakil Khan <shakil10945@gmail.com>
2024-02-08 02:37:33 +0600

    second

--- Problem_01.js
+++ Problem_01.js
@@ -1,2 +1,12 @@
-//console.log("helllo");
-console.log("Hello Shakilllllllllllllllllllll");
+function calculateMoney(ticketSale) {
+    if (ticketSale>=0){
+        remainingMoney= (ticketSale *120) - (500 + (8*50));
+        return remainingMoney;
+    }
+    else{
+        return 'Invalid Number';
+    }
+    
+}
+
+console.log(calculateMoney(0));
